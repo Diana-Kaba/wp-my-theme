b4e27c80ae3bcdcf0bc49f5f4b7dbcccaa6b42f6
Diana-Kaba <kabadiana327@gmail.com>
2024-05-14 17:02:54 +0300

    added caching

--- wp-content/themes/bootdi/includes/enqueue.php
+++ wp-content/themes/bootdi/includes/enqueue.php
@@ -3,17 +3,15 @@
 function bootdi_enqueue()
 {
     $url = get_theme_file_uri();
-    wp_register_style('bootdi_google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap'); // Google fonts
-    wp_register_style('bootdi_bootstrap', $url . '/assets/vendor/bootstrap/css/bootstrap.min.css'); // bootstrap.min.css
-    wp_register_style('bootdi_modern_business', $url . '/assets/css/modern-business.css'); // modern-business.css
+    $ver = BOOTSTRAPTOPIC_DEV_MODE ? time() : false;
+    wp_register_style('bootstraptopic_google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap', [], $ver); // Google fonts
+    wp_register_style('bootstraptopic_bootstrap', $url . '/assets/vendor/bootstrap/css/bootstrap.min.css', [], $ver); // bootstrap.min.css
+    wp_register_style('bootstraptopic_modern_business', $url . '/assets/css/modern-business.css', [], $ver); // modern-business.cs
+    wp_register_script('bootstraptopic_bootstrap', $url . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', 'jquery', $ver, true);
 
-    wp_enqueue_style('bootdi_google_fonts');
-    wp_enqueue_style('bootdi_bootstrap');
-    wp_enqueue_style('bootdi_modern_business');
-
-    wp_register_script('bootdi_bootstrap', $url .
-        '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', [], false, true);
-
+    wp_enqueue_style('bootstraptopic_bootstrap');
+    wp_enqueue_style('bootstraptopic_modern_business');
+    wp_enqueue_style('bootstraptopic_google_fonts');
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bootdi_bootstrap');
+    wp_enqueue_script('bootstraptopic_bootstrap');
 }
