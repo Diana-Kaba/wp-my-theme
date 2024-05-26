0949fb405fea6b09a7a7210a87b55a9a4773abfb
Diana-Kaba <kabadiana327@gmail.com>
2024-05-26 15:17:19 +0300

    added wp-query

--- wp-content/themes/bootdi/single.php
+++ wp-content/themes/bootdi/single.php
@@ -100,11 +100,11 @@ get_header();?>
       <!-- Post Content Column -->
       <div class="col-lg-8">
         <?php if (have_posts()) {
-            while (have_posts()) {
-            the_post();
-            global $post;
-            $author_ID = $post->post_author;
-            $author_URL = get_author_posts_url($author_ID);
+    while (have_posts()) {
+        the_post();
+        global $post;
+        $author_ID = $post->post_author;
+        $author_URL = get_author_posts_url($author_ID);
         ?>
         <!-- Title -->
         <h1 class="mt-4"><?php the_title()?></h1>
@@ -125,7 +125,7 @@ get_header();?>
         <hr>
         <!-- Preview Image -->
         <?php
-        if (has_post_thumbnail()) {
+if (has_post_thumbnail()) {
             the_post_thumbnail("full", ["class" => "card-img-top"]);
         }
         ?>
@@ -141,9 +141,9 @@ get_header();?>
         ?>
         <hr>
         <!-- Tag cloud -->
-        <?php if(has_tag()) {
-        the_tags("", ", ");
-        echo "<hr>";
+        <?php if (has_tag()) {
+            the_tags("", ", ");
+            echo "<hr>";
         }
         ?>
         <!-- Pagination -->
@@ -156,17 +156,17 @@ get_header();?>
                 </li>
             </ul>
 
-        <?php edit_post_link(); ?>
+        <?php edit_post_link();?>
 
         <!-- Comments Form -->
         <?php
-        if (comments_open() || get_comments_number()) {
+if (comments_open() || get_comments_number()) {
             comments_template();
         }
         ?>
 
-        <!-- Post Author Info -->
-        <div class="card mb-4">
+                <!-- Post Author Info -->
+                <div class="card mb-4">
                 <div class="card-header mb-2">
                     <strong>
                         Posted by
@@ -182,11 +182,73 @@ get_header();?>
             </div>
         <!-- Post Single - Author End -->
 
+        <h4>Related Posts
+                <?php the_category()?>
+            </h4>
+            <div class="related-posts clearfix">
+                <?php
+$categories = get_the_category();
+        $rp_query = new WP_Query([
+            'posts_per_page' => 2,
+            'post__not_in' => [$post->ID],
+            'cat' => !empty($categories) ? $categories[0]->term_id : null,
+        ]);
+        if ($rp_query->have_posts()) {
+            while ($rp_query->have_posts()) {
+                $rp_query->the_post();
+                ?>
+                <div class="mpost clearfix">
+                    <h5><?php the_category();?></h5>
+                    <?php
+if (has_post_thumbnail()) {
+                    ?>
+                    <div class="entry-image">
+                        <a href="<?php the_permalink();?>">
+                            <?php the_post_thumbnail('thumbnail');?>
+                        </a>
+                    </div>
+                    <?php
+}
+                ?>
+                    <div class="entry-c">
+                        <div class="entry-title">
+                            <h4>
+                                <a href="<?php the_permalink();?>">
+                                    <?php the_title();?>
+                                </a>
+                            </h4>
+                        </div>
+                        <ul class="entry-meta clearfix">
+                            <li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
+                            <li><i class="icon-comments"></i> <?php comments_number('0');?></li>
+                        </ul>
+                        <div class="entry-content">
+                            <?php the_excerpt();?>
+                        </div>
+                    </div>
+                </div>
+                <?php
+}
+            wp_reset_postdata();
+            $query = new WP_Query(['category_name' => 'news']);
+            while ($query->have_posts()) {
+                $query->the_post();?>
+            <!-- <ul> -->
+            <li> <a href="<?php the_permalink();?>">
+                    <?php the_title();?>
+                </a></li>
+            <!-- </ul> -->
+            <?php
+}
+        }
+        ?>
+            </div>
+
       </div>
       <?php
-        }
-        }
-      ?>
+}
+}
+?>
       <!-- Sidebar Widgets Column -->
       <div class="col-md-4">
         <?php get_sidebar();?>
