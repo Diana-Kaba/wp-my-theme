adfcb067865947e157cf566cb45400dab3322e79
Diana-Kaba <kabadiana327@gmail.com>
2024-05-26 16:05:58 +0300

    added security for plugin

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -1,9 +1,2 @@
 <?php
-/**
- * Plugin Name: Recipe
- * * Description: Creating the recipes and rate those recipes
- * Version: 1.0
- * Author: Diana Kaba
- * Author URI: https://github.com/Diana-Kaba
- * Text Domain: recipe
- */
+# Silence is golden.
